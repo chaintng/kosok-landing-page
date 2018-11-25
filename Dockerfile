655bafb2e4092dd12c9e98eb928dc6ab5feec5cf
Chainarong Tangsurakit <chaintng@gmail.com>
2018-11-25 23:17:20 +0700

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM node:10.13.0-alpine
+
+RUN apk add --no-cache --virtual .persistent-deps \
+        curl \
+        openssl \
+        # for node-sass module
+        make \
+        gcc \
+        g++ \
+        python \
+        py-pip \
+    # Install node packages
+    && npm install --silent --save-dev -g \
+        typescript serve
+
+WORKDIR /app
+
+COPY package.json package.json
+COPY yarn.lock yarn.lock
+
+RUN yarn install
+
+COPY . .
+
+RUN yarn build
+
+ENTRYPOINT ["serve", "-s", "build"]
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,60 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+import leading from './images/leading2.jpg';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div className="main-navigation">
+          <div className="wrapper">
+            <div className="logo">kosok.co</div>
+              <ul className="MainNavigation-items">
+                <li className="MainNavigation-item"><a className="MainNavigation-itemButton"
+                                                       href="/en/online-audio-mastering/">
+                  <div>Mastering</div>
+                </a></li>
+                <li className="MainNavigation-item"><a className="MainNavigation-itemButton"
+                                                       href="/en/digital-distribution/">
+                  <div>Distribution</div>
+                </a></li>
+                <li className="MainNavigation-item"><a href="https://samples.landr.com" target="_top" rel="noopener"
+                                                       className="MainNavigation-itemButton">
+                  <div>Samples</div>
+                </a></li>
+                <li className="MainNavigation-item"><a className="MainNavigation-itemButton" href="/en/pricing/">
+                  <div>Pricing</div>
+                </a></li>
+                <li className="MainNavigation-item"><a href="https://blog.landr.com/" target="_top" rel="noopener"
+                                                       className="MainNavigation-itemButton">
+                  <div>Blog</div>
+                </a></li>
+                <li className="MainNavigation-item"><a href="https://support.landr.com/" target="_top" rel="noopener"
+                                                       className="MainNavigation-itemButton">
+                  <div>Help</div>
+                </a></li>
+                <li className="MainNavigation-item MainNavigation-item--secondary"><a
+                  className="MainNavigation-itemButton" href="/en/login/">
+                  <div>Log in</div>
+                </a></li>
+                <li className="MainNavigation-item MainNavigation-item--secondary Sign-up"><span
+                  className="u-hidden-lg-down"><a className="MainNavigation-itemButton-signUp"
+                                                  href="/en/signup/?homePage=Home"><div
+                  className="Button Button--primary Button--xsmall"><div className="Button-wrapper"><div
+                  className="Button-text">Sign up</div></div></div></a></span></li>
+              </ul>
+          </div>
+        </div>
+        <div className="site-body">
+          <div className="wrapper">
+            <div className="Hero-content-title"><h1
+              className="Heading Heading--mg">Create, we'll do the rest. </h1></div>
+            <div className="Hero-content-description"><p>A podcast solution for podcast creator.
+              Since first step until monetization.</p></div>
+          </div>
+          <img src={leading} className="hero-lead" alt="logo" />
+        </div>
       </div>
     );
   }
