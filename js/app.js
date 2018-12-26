765e77bbffc5253becdb6102e409ea297277f167
Chainarong Tangsurakit <chaintng@gmail.com>
2018-12-26 17:03:00 +0700

    typo

--- js/app.js
+++ js/app.js
@@ -47,38 +47,6 @@
         });
     },
 
-    KivoxApp.prototype.initCounter = function() {
-        var a = 0;
-        $(window).scroll(function() {
-            var oTop = $('#counter').offset().top - window.innerHeight;
-            if (a == 0 && $(window).scrollTop() > oTop) {
-                $('.counter-value').each(function() {
-                    var $this = $(this),
-                        countTo = $this.attr('data-count');
-                    $({
-                        countNum: $this.text()
-                    }).animate({
-                            countNum: countTo
-                        },
-
-                        {
-                            duration: 2000,
-                            easing: 'swing',
-                            step: function() {
-                                $this.text(Math.floor(this.countNum));
-                            },
-                            complete: function() {
-                                $this.text(this.countNum);
-                                //alert('finished');
-                            }
-
-                        });
-                });
-                a = 1;
-            }
-        });
-    },
-
     KivoxApp.prototype.initTestiMonial = function() {
          $("#owl-demo").owlCarousel({
             autoPlay: 3000,
@@ -158,7 +126,6 @@
         this.initScrollspy();
         this.initTestiMonial();
         this.initMfpVideo();
-        this.initCounter();
         this.initContact();
         this.initBacktoTop();
     },
